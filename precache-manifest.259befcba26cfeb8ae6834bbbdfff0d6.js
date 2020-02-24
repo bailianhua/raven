62f434aa9a99760d73cc08b3694629b9d98f3b1f
korrakoch <korrakoch.on@aerothai.co.th>
2020-02-24 15:43:28 +0700

    deploy

---
+++ precache-manifest.259befcba26cfeb8ae6834bbbdfff0d6.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1702dbfa5c02f07fb363670ce2d10a9b",
+    "url": "/raven/404.html"
+  },
+  {
+    "revision": "cf7392510ccfbd5ea45a",
+    "url": "/raven/css/chunk-380e5122.7d0030cb.css"
+  },
+  {
+    "revision": "9a19602c62e4d2e26552",
+    "url": "/raven/css/chunk-f1532b9a.bb4a451e.css"
+  },
+  {
+    "revision": "a8f97c1f61458318d27f",
+    "url": "/raven/css/chunk-vendors.5a95c14b.css"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/raven/favicon_.ico"
+  },
+  {
+    "revision": "15cef945c38957a79e996774fff4b1dc",
+    "url": "/raven/img/classes/assassin.jpg"
+  },
+  {
+    "revision": "1702b013f347388b954476d38c3c77fc",
+    "url": "/raven/img/classes/blademaster.jpg"
+  },
+  {
+    "revision": "5c8a89fd667696542566b9fbfe14c1c3",
+    "url": "/raven/img/classes/classes_bg.jpg"
+  },
+  {
+    "revision": "a7c533053df934ae74261833158ab763",
+    "url": "/raven/img/classes/cy_btn.png"
+  },
+  {
+    "revision": "62bbb425ff97462eb1c15bc41759e114",
+    "url": "/raven/img/classes/gunslinger.jpg"
+  },
+  {
+    "revision": "5ad03e0abe133784893fc1c336a9633a",
+    "url": "/raven/img/classes/ly_btn.png"
+  },
+  {
+    "revision": "46e1d8b936b66bbec32ce2bf6daceca8",
+    "url": "/raven/img/classes/souldancer.jpg"
+  },
+  {
+    "revision": "f7c897c53b0d4eeb94fa913c3169a4be",
+    "url": "/raven/img/classes/ss_btn.png"
+  },
+  {
+    "revision": "97b3126e1cf0631b1815189cf88656bf",
+    "url": "/raven/img/classes/zxz_btn.png"
+  },
+  {
+    "revision": "83739a5002ce5fa17c11a9991361a03a",
+    "url": "/raven/img/dragonraja-bg.jpg"
+  },
+  {
+    "revision": "642f070e8f93125c37af4797efbfc98f",
+    "url": "/raven/index.html"
+  },
+  {
+    "revision": "52523e5011412e0072de",
+    "url": "/raven/js/app.5cd2acce.js"
+  },
+  {
+    "revision": "d7dd80314ec0aa826376",
+    "url": "/raven/js/chunk-2d21a3d2.69779e88.js"
+  },
+  {
+    "revision": "712d785adbf4c1c969df",
+    "url": "/raven/js/chunk-2d22d746.9ef59c6a.js"
+  },
+  {
+    "revision": "cf7392510ccfbd5ea45a",
+    "url": "/raven/js/chunk-380e5122.ac258b14.js"
+  },
+  {
+    "revision": "fa3f342c5897e9512d7c",
+    "url": "/raven/js/chunk-74acf1a7.50b9534c.js"
+  },
+  {
+    "revision": "9a19602c62e4d2e26552",
+    "url": "/raven/js/chunk-f1532b9a.4afbaa53.js"
+  },
+  {
+    "revision": "a8f97c1f61458318d27f",
+    "url": "/raven/js/chunk-vendors.c54d0fa5.js"
+  },
+  {
+    "revision": "9be48e2275faafdbf5fcf8c7de97c594",
+    "url": "/raven/manifest.json"
+  },
+  {
+    "revision": "06d4d7ebeedcb0bd27381f23f2e1a873",
+    "url": "/raven/ravenLogo.png"
+  },
+  {
+    "revision": "4ab6c396f99b74ef71551d9990391cdf",
+    "url": "/raven/ravenlogo.ico"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/raven/robots.txt"
+  }
+]);
